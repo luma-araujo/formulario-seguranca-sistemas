f117d8c7c5f6140ea1f0cf745cd6aa526905baed
Luma <lumaoliver71@gmail.com>
2025-07-08 21:14:05 -0300

    Atualização do teste-bcrypt.js

--- teste-bcrypt.js
+++ teste-bcrypt.js
@@ -1,10 +1,10 @@
 const bcrypt = require('bcrypt');
 
-const hash = '$2b$10$zoGuxYv7C0dc/ahVxEatNuzhoyjkNxTJDCGGcUPX9a8f7hVEcm/wa';
-const senhaTeste = 'Senha1234!'; 
+const hash = '$2b$10$K0Y9f85qiUY3YvglsunuV.2Xlt4CE5X99Y3jw94C33k6ie0E5c8hC';
+const senhaTeste = '@Senha1234'; 
 
 bcrypt.compare(senhaTeste, hash).then(result => {
-  console.log('Senha bate com hash?', result); // true ou false
+  console.log('Senha bate com hash?', result); // True ou false
 }).catch(err => {
   console.error('Erro:', err);
 });
